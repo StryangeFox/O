860d668485c476cbba3bde41f5204d2c866d2dc7
StryangeFox <58075234+StryangeFox@users.noreply.github.com>
2020-12-27 18:35:56 +0700

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,68 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+#define MAXMODULE 50
+
+typedef void (WINAPI* cfunc)();
+
+cfunc Matrix;
+cfunc Funk;
+
+void main()
+{
+    HINSTANCE hLib = LoadLibrary(TEXT("DLL1.DLL"));
+    if (hLib == NULL)
+    {
+        cout << "Unable to load library!" << endl;
+        _getch();
+        return;
+    }
+
+    char mod[MAXMODULE];
+
+    GetModuleFileName((HMODULE)hLib, (LPTSTR)mod, MAXMODULE);
+    cout << "Library loaded: " << mod << endl;
+    Matrix = (cfunc)GetProcAddress((HMODULE)hLib, "Matrix");
+    Funk = (cfunc)GetProcAddress((HMODULE)hLib, "Funk");
+    if ((Matrix == NULL) || (Funk == NULL))
+    {
+        cout << "Unable to load function(s)." << endl;
+        FreeLibrary((HMODULE)hLib);
+        return;
+    }
+    Matrix();
+    cout << endl;
+    Funk();
+    cout << endl;
+    FreeLibrary((HMODULE)hLib);
+    _getch();
+}
+
+
+/*
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    typedef void (*VoidReturnFunc)();
+    HINSTANCE a = LoadLibrary(TEXT("Dll1.dll"));
+    if (a == NULL) cout << "error1";
+    if (a != NULL) cout << "it's working";
+
+    VoidReturnFunc funk = (VoidReturnFunc) GetProcAddress(a, "funk");
+    if (a == NULL) cout << "error2";
+    if (a != NULL) funk();
+
+
+    _getch();
+    FreeLibrary(a);
+    return 0;
+}
+*/
---
+++ ConsoleApplication2/ConsoleApplication2/dllmain.cpp
@@ -0,0 +1,88 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+
+extern "C" __declspec(dllexport) void  Matrix()
+{
+
+    int n, m, buf, i, j;
+    int a[12][12];
+    cout << "Input N ";
+    cin >> n;
+    m = n;
+    cout << endl;
+    srand(time(NULL));
+    for (i = 0;i < n;i++) {
+        for (j = 0;j < m;j++) {
+
+            a[i][j] = -100 + rand() % 100;
+        }
+
+    }
+    for (i = 0;i < n;i++) {
+        for (j = 0;j < m;j++) {
+            cout << a[i][j] << " ";
+
+        }
+        cout << endl;
+    }
+    system("pause");
+
+}
+
+extern "C" __declspec(dllexport) void  Funk()
+{
+
+    cout << "Hey world";
+    system("pause<void");
+
+}
+
+#define MAXMODULE 50
+
+char module[MAXMODULE];
+
+extern "C" __declspec(dllexport) void NumberList()
+{
+    GetModuleFileName(NULL, (LPTSTR)module, MAXMODULE);
+    cout << "\n\nThis function was called from "
+        << module
+        << endl << endl;
+    cout << "NumberList(): ";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl << endl;
+}
+
+extern "C" __declspec(dllexport) void LetterList()
+{
+    GetModuleFileName(NULL, (LPTSTR)module, MAXMODULE);
+    cout << "\n\nThis function was called from "
+        << module
+        << endl << endl;
+    cout << "LetterList(): ";
+    for (int i = 0; i < 26; i++)
+    {
+        cout << char(97 + i) << " ";
+    }
+    cout << endl << endl;
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
